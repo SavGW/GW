1dc7392499dbc7b4661e02bf006206d3ed9d2f2f
SavGW <softmaker2010@gmail.com>
2014-12-21 22:34:02 +1100

    Update wikiItem[GW].user.js

--- wikiItem[GW].user.js
+++ wikiItem[GW].user.js
@@ -12,7 +12,7 @@
 //
 // @include         http://www.ganjawars.ru/item.php?*
 //
-// @version         1.0
+// @version         1.1
 // @updateURL		    https://raw.github.com/SavGW/GW/master/wikiItem[GW].user.js
 // ==/UserScript==
 
